5fe39ea68031beb8055fa2f748e2bb74c0d1983d
Takanori Watanabe <takanori5064@gmail.com>
2019-05-08 19:08:07 +0900

    [add]NNの練習追加

--- machine-lerning/deep-learning-from-scratch/ch03/ex.py
+++ machine-lerning/deep-learning-from-scratch/ch03/ex.py
@@ -24,3 +24,37 @@ print(B)
 np.ndim(B)
 B.shape
 #%%
+import numpy as np
+A = np.array([[1,2], [3,4]])
+A.shape
+B = np.array([[5,6], [7,8]])
+B.shape
+np.dot(A, B)
+#%%
+import numpy as np
+A = np.array([[1,2,3],[4,5,6]])
+A.shape
+B = np.array([[1,2],[3,4],[5,6]])
+B.shape
+np.dot(A,B)
+#%%
+import numpy as np
+X = np.array([1, 2])
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+Y = np.dot(X, W)
+print(Y)
+#%%
+import numpy as np
+def sigmoid_func(x):
+    return 1 / (1 + np.exp(-x))
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid_func(A1)
+print(Z1)
+
+#%%
